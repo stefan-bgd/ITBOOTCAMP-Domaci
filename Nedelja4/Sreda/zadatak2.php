4140a33e7ab31b3f70b8408e2497abce3b62cabe
stefan-bgd <stjovanovicbg@gmail.com>
2021-05-12 16:23:29 +0200

    Domaci nedelja 4

---
+++ Nedelja4/Sreda/zadatak2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadatak 2</title>
+</head>
+<body>
+    
+     <?php
+      
+      #Date su 2 reci na engleskom koje predstavljaju 2 boje (izaberite sami). #Prikazati svaku rec preko spana u boji koju predstavlja. (moze samo inline)
+     
+    
+      $red = "red";
+      $green = "green";
+
+      echo "<span style='color:$red'>RED</span>";
+      echo "<br>";
+      echo "<span style='color:$green'>GREEN</span>";
+
+
+     ?>
+
+</body>
+</html>
