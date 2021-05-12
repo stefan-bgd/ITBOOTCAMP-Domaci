a514a67e12b18e222f7181f30cadebf2ee4f879d
stefan-bgd <stjovanovicbg@gmail.com>
2021-05-12 16:04:53 +0200

    Domaci za sredu nedelja 4

---
+++ Nedelja 4/Sreda/domaci_12/zadatak2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadatak 2 Link</title>
+    <?php   
+         $link = "http://localhost/ITBOOTCAMP/12D_6804/zadatak2.php";      
+    ?>
+   
+</head>
+<body>
+
+    <a href="<?= $link ?>"> ZADATAK 2  </a>
+    
+</body>
+</html>
